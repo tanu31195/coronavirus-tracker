5b580eca8baa5d3a9285900babee04baab31e35b
Tanushka PC <tanu31195@users.noreply.github.com>
2020-02-29 12:23:12 +0530

    Initial commit

---
+++ src/main/java/io/tanu31195/coronavirustracker/services/CoronaVirusDataService.java
@@ -0,0 +1,10 @@
+package io.tanu31195.coronavirustracker.services;
+
+public class CoronaVirusDataService {
+
+    private static String VIRUS_DATA_URL = "";
+
+    public void fetchVirusData() {
+
+    }
+}
---
+++ src/test/java/io/tanu31195/coronavirustracker/CoronavirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tanu31195.coronavirustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
